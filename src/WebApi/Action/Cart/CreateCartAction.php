893a73048ec00c9f8c74e9600a37b560f24b82ed
Alex <alzholus@gmail.com>
2021-08-14 20:41:49 +0200

    Add an empty api actions

---
+++ src/WebApi/Action/Cart/CreateCartAction.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\WebApi\Action\Cart;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class CreateCartAction extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return new Response(__CLASS__);
+    }
+}
---
+++ src/WebApi/Action/Product/ShowProductListAction.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\WebApi\Action\Product;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ShowProductListAction extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return new Response(__CLASS__);
+    }
+}
